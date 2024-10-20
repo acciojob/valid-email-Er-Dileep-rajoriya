b630029a27aba4274ac5b773eb06b6b3c0d5bc09
Dileep Rajoriya <139870968+Er-Dileep-rajoriya@users.noreply.github.com>
2024-10-21 03:06:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function validEmail(str) {
-  //your JS code here.
+  // Regular expression to validate the email format
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
+  
+  // Test the input string against the regex pattern
+  return emailPattern.test(str);
 }
 
 // Do not change the code below.
